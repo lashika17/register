e4841b177c45c45da8dd2cd676b98afbe015a6aa
lashika17 <91522934+lashika17@users.noreply.github.com>
2023-03-05 16:01:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const form_validate=()=>{
+    const name=document.getElementById("name").value;
+    const email=document.getElementById("email").value;
+    const psw=document.getElementById("password").value;
+    const dob=document.getElementById("dob").value;
+    const check=document.getElementById("logCheck").checked;
+            var Entries;
+            if(localStorage.getItem('user-entries')===null)
+            Entries=[];
+            else
+            Entries=JSON.parse(localStorage.getItem('user-entries'))
+            Entries.unshift({
+                name:name,
+                email:email,
+                psw:psw,
+                dob:dob,
+                check:check
+            })
+            localStorage.setItem('user-entries',JSON.stringify(Entries));
+            showdatas()
+           }
+        const showdatas=()=>{
+    var Entries;
+    if(localStorage.getItem('user-entries')===null)
+    Entries=[];
+    else
+    Entries=JSON.parse(localStorage.getItem('user-entries'))
+    
+    var html="";
+    
+    Entries.forEach((element,intex)=>{
+    html += "<tr>";
+    html += "<td>" + element.name+"</td>";
+    html += "<td>" + element.email+"</td>";
+    html += "<td>" + element.psw+"</td>";
+    html += "<td>" + element.dob+"</td>";
+    html += "<td>" + element.check+"</td>";
+    html += "</tr>";
+    });
+    document.querySelector("#regTable tbody").innerHTML=html;
+    
+    }
+    document.onload=showdatas();    
+        
